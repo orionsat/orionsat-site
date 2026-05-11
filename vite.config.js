bad7bf6e3d53b5c1c07e34bd09c9d770b0c129a1
orionsat <adm@orionsatgestao.com.br>
2026-05-11 01:46:08 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['orionsat-frontend.9uo8j1.easypanel.host']
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: ['orionsat-frontend.9uo8j1.easypanel.host']
+  }
 })
